9546b7dd4640b2688a70cd2f3ab1ac801443806d
Mikael Mattsson <62009528+mikael-mattsson@users.noreply.github.com>
2022-03-20 20:39:31 +0100

    Add and setup express-handlebars view engine

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
 import express from 'express';
+import { engine } from 'express-handlebars';
 
 const app = express();
+
+app.engine('handlebars', engine());
+app.set('view engine', 'handlebars');
 
 app.get('/', (req, res) => res.send('Hello World!'));
 
